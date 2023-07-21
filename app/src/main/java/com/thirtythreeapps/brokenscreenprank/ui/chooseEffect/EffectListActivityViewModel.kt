f5c9bcd8158b5ddea803e309c06973a203607ecb
Hamza rafique <ameer.hamza6733@gmail.com>
2023-07-21 15:25:50 +0500

    PrankDetailActivity.kt

--- app/src/main/java/com/thirtythreeapps/brokenscreenprank/ui/chooseEffect/EffectListActivityViewModel.kt
+++ app/src/main/java/com/thirtythreeapps/brokenscreenprank/ui/chooseEffect/EffectListActivityViewModel.kt
@@ -8,14 +8,12 @@ class EffectListActivityViewModel() :
 
     fun getCrackEffect(): List<EffectModel> {
         return listOf<EffectModel>(
-            EffectModel(effectDrawableImage = R.drawable.first_crack_effect_preview),
-            EffectModel(effectDrawableImage = R.drawable.second_crack_effect_preview),
+            EffectModel(effectDrawableImage = R.drawable.first_crack_effect),
+            EffectModel(effectDrawableImage = R.drawable.second_crash_effect_preview),
             EffectModel(effectDrawableImage = R.drawable.thired_crack_effect_preview),
             EffectModel(effectDrawableImage = R.drawable.forth_crack_effect_preview),
-            EffectModel(effectDrawableImage = R.drawable.fifth_crack_effect_preview),
-            EffectModel(effectDrawableImage = R.drawable.sixth_broken_screen_preview),
             EffectModel(effectDrawableImage = R.drawable.seventh_broken_screen_preview),
-            EffectModel(effectDrawableImage = R.drawable.eighth_broken_screen_preview)
+            EffectModel(effectDrawableImage = R.drawable.eight_broken_screen_preview)
         )
     }
 }
