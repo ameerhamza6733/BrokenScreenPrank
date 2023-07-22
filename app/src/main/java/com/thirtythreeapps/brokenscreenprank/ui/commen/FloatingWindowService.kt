3ae71c237869ada8270dc40998b5b1b0c31b0b72
Hamza rafique <ameer.hamza6733@gmail.com>
2023-07-22 15:23:36 +0500

    working on crack

---
+++ app/src/main/java/com/thirtythreeapps/brokenscreenprank/ui/commen/FloatingWindowService.kt
@@ -0,0 +1,135 @@
+package com.thirtythreeapps.brokenscreenprank.ui.commen
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.graphics.PixelFormat
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import android.view.*
+import android.widget.ImageView
+import androidx.core.app.NotificationCompat
+import com.bumptech.glide.Glide
+import com.thirtythreeapps.brokenscreenprank.R
+
+
+class FloatingWindowService : Service(), View.OnTouchListener {
+    val TAG = "FloatingUiMangerBaseTAG"
+    private lateinit var context: Context
+    private lateinit var windowManager: WindowManager
+    private var floatingView: View? = null
+    private var screenWidth: Int = 0
+    private var screenHeight: Int = 0
+
+    override fun onCreate() {
+        super.onCreate()
+        createForegroundNotification()
+        screenWidth = application.resources.displayMetrics.widthPixels
+        screenHeight = application.resources.displayMetrics.heightPixels
+
+    }
+
+    private fun create(context: Context) {
+        this.context = context
+        windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    }
+
+
+    private fun showFloatingWindow(layout: Int, params: WindowManager.LayoutParams) {
+        floatingView?.let {
+            windowManager.removeView(it)
+        }
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        floatingView = inflater.inflate(layout, null)
+        params!!.gravity = Gravity.TOP or Gravity.START
+        windowManager.addView(floatingView, params)
+
+        floatingView!!.setOnClickListener {
+            showFloatingWindow(
+                R.layout.floating_layout,
+                getNonClickAbleLayoutParams(width = screenWidth, height = screenHeight)
+            )
+            val crackImage = floatingView?.findViewById<ImageView>(R.id.ivCrackEffect)
+            crackImage?.visibility = View.VISIBLE
+            Glide.with(this).asBitmap().load(R.drawable.crack_effect_1).into(crackImage!!)
+        }
+    }
+
+    private fun getNonClickAbleLayoutParams(width: Int, height: Int): WindowManager.LayoutParams {
+        return WindowManager.LayoutParams(
+            width, height,
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+            } else {
+                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT
+            },
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                    or WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+            PixelFormat.TRANSLUCENT
+        )
+    }
+
+    private fun getClickAbleLayoutParams(width: Int, height: Int): WindowManager.LayoutParams {
+        return WindowManager.LayoutParams(
+            width, height,
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+            } else {
+                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT
+            },
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                    or WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+            PixelFormat.TRANSLUCENT
+        )
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        // Start the service in the foreground, which requires a notification
+        create(this)
+
+        showFloatingWindow(
+            R.layout.floating_layout,
+            getClickAbleLayoutParams(screenWidth, screenHeight)
+        )
+        return START_STICKY
+    }
+
+    private fun createForegroundNotification() {
+        // Create a notification channel for Android Oreo and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "ForegroundServiceChannel"
+            val channel = NotificationChannel(
+                channelId,
+                "Foreground Service Channel",
+                NotificationManager.IMPORTANCE_LOW
+            )
+            val manager = getSystemService(
+                NotificationManager::class.java
+            )
+            manager?.createNotificationChannel(channel)
+        }
+
+        // Create the notification to show the foreground service
+        val notificationBuilder: NotificationCompat.Builder =
+            NotificationCompat.Builder(this, "ForegroundServiceChannel")
+                .setContentTitle("Foreground Service")
+                .setContentText("Touch events are being passed through the floating window.")
+                .setSmallIcon(R.drawable.baseline_circle_notifications_24)
+
+        // Start the service in the foreground with the notification
+        startForeground(1, notificationBuilder.build())
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        Log.d(TAG, "ontouch")
+
+        return false
+    }
+}
--- app/src/main/java/com/thirtythreeapps/brokenscreenprank/ui/prankDetailScreen/PrankDetailActivity.kt
+++ app/src/main/java/com/thirtythreeapps/brokenscreenprank/ui/prankDetailScreen/PrankDetailActivity.kt
@@ -1,24 +1,41 @@
 package com.thirtythreeapps.brokenscreenprank.ui.prankDetailScreen
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment.STYLE_NO_TITLE
 import com.bumptech.glide.Glide
 import com.google.android.gms.ads.AdRequest
 import com.thirtythreeapps.brokenscreenprank.R
 import com.thirtythreeapps.brokenscreenprank.databinding.ActivityPrankDetailBinding
 import com.thirtythreeapps.brokenscreenprank.ui.chooseEffect.effectFromJson
-import com.thirtythreeapps.brokenscreenprank.ui.commen.FloatingUiMangerBase
+import com.thirtythreeapps.brokenscreenprank.ui.commen.FloatingWindowService
 import com.thirtythreeapps.brokenscreenprank.ui.commen.PrankType
+
 
 class PrankDetailActivity : AppCompatActivity() {
     private val TAG ="PrankDetailActivity"
     private lateinit var binding : ActivityPrankDetailBinding
     private val viewModel by viewModels<PrankDetailViewModel> ()
+    private val requestNotificationPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted->
+            if (isGranted) {
+                showFloadtingWin()
+            }
+            else {
+
+            }
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPrankDetailBinding.inflate(layoutInflater)
@@ -64,8 +81,11 @@ class PrankDetailActivity : AppCompatActivity() {
 
         binding.btStartPrank.setOnClickListener {
             if (Settings.canDrawOverlays(this)){
-                val floatingUiMangerBase = FloatingUiMangerBase(this)
-                floatingUiMangerBase.showFloatingUI(R.layout)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    checkNotificationPermission()
+                }else{
+                    showFloadtingWin()
+                }
             }else{
                 showDialog()
             }
@@ -77,6 +97,31 @@ class PrankDetailActivity : AppCompatActivity() {
 
     }
 
+    private fun showFloadtingWin(){
+        val floatingIntent = Intent(this, FloatingWindowService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(floatingIntent)
+        } else {
+            startService(floatingIntent)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    fun checkNotificationPermission() {
+        val permission = Manifest.permission.POST_NOTIFICATIONS
+        when {
+            ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED -> {
+                // make your action here
+                showFloadtingWin()
+            }
+            shouldShowRequestPermissionRationale(permission) -> {
+
+            }
+            else -> {
+                requestNotificationPermission.launch(permission)
+            }
+        }
+    }
     fun showDialog() {
         val fragmentManager = supportFragmentManager
         val newFragment = EnableOverLayAlertDialog()
