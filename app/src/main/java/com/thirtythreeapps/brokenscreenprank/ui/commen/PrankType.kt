ec37803507f215ee7770337bf3785c1bcfa2ff9b
DAVIDS <dpprimegithub@gmail.com>
2023-08-02 13:57:28 +0300

    new images 1. release to google play

--- app/src/main/java/com/thirtythreeapps/brokenscreenprank/ui/commen/PrankType.kt
+++
@@ -1,5 +0,0 @@
-package com.thirtythreeapps.brokenscreenprank.ui.commen
-
-object PrankType{
-    val  BROKEN_SCREEN_PRANK = 1
-}
--- app/src/main/java/com/thirtythreeapps/brokenscreenprank/ui/home/PrankModel.kt
+++
@@ -1,3 +0,0 @@
-package com.thirtythreeapps.brokenscreenprank.ui.home
-
-data class PrankModel(val title:String, val type:Int)
